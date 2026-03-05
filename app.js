76bc2cce59258e9daf5bcf3e5c5a1ce42e188993
NafisatB <nafisatbabamusa@outlook.com>
2026-03-05 12:54:50 +0100

    setup project

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors('*'));
+
+let noteTaking = [
+    {
+        id: 1, 
+        title: "The board meeting notes",
+        content: "Discuss project milestones and deadlines",
+        createdDate: "2026-02-20",
+        noteCompleted: true
+    },
+
+    {
+        id: 2, 
+        title: "Church message",
+        content: "Describe the faithfulness of God", 
+        createdDate: "2026-03-02",
+        noteCompleted: false
+    },
+
+
+    {
+        id: 3,
+        title: "President speech on drug dealers",
+        content: "Methods to combat illegal drugs sellers",
+        createdDate: "2025-08-20",
+        noteCompleted: true
+
+    }
+];
